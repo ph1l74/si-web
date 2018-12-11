4eed80608721a8309782cd5dad41b46f5ebfe260
Filat Astakhov <astakhovfilat@gmail.com>
2018-12-11 15:00:05 +0500

    Добавлена загрузка в кукис текущего раунда и вопроса

--- scripts/app.js
+++ scripts/app.js
@@ -50,9 +50,11 @@ function clearPlayerList() {
     playerList.empty();
 }
 
+
+// check if name already in game
 function checkNames(playerName) {
     for (var player in cStats) {
-        if(player == playerName){
+        if (player == playerName) {
             return true;
         }
     }
@@ -81,8 +83,13 @@ function addPlayer() {
 // clearing all results from cStats and writing it to Cookie
 function clearResults() {
     if (confirm("Вы действительно хотите очистить результаты?")) clearPlayerList();
-    cStats = {}
+    cStats = {};
+    curRound = 1;
+    curCost = 10;
     setCookie('stats', JSON.stringify(cStats), 2);
+    // setCookie('round', curRound + "", 2);
+    // setCookie('cost', curCost + "", 2);
+    updateRoundInfo();
 }
 
 
@@ -100,16 +107,22 @@ function scoreMinus(e) {
     // console.log(cStats);
     // $('#' + e.target.parentNode.id + '_scores').text(cStats[e.target.parentNode.id].scores -= curCost);
     // $('#' + e.target.parentNode.id + '_scores').text(cStats[e.target.parentNode.id].scores -= curCost);
-    
+
     $(e.target).nextAll('.playerScores').text(cStats[e.target.parentNode.id].scores -= curCost);
     setCookie('stats', JSON.stringify(cStats), 2);
 }
 
+
+// updateing text and setting in Cookie
 function updateRoundInfo() {
     $('#statusRoundSpan').text(curRound);
     $('#statusCostSpan').text(curCost);
+    setCookie('round', curRound + "", 2);
+    setCookie('cost', curCost + "", 2);
 }
 
+
+// change cost and if cost 50 increment the round
 function nextQuestion() {
     if (curCost != 50) curCost += 10;
     else {
@@ -121,21 +134,34 @@ function nextQuestion() {
 
 
 window.onload = function () {
+    // Getting last session info
     cStats = getCookie('stats');
+    curRound = getCookie('round');
+    curCost = getCookie('cost');
+
+    // If not remember: start new game
     if (cStats == "" || !cStats) {
-        console.log('No previous game. Creating new player list...')
-        cStats = {}
+        cStats = {};
     }
+
+    else if (curRound == "" || !curRound || curCost == "" || !curCost) {
+        curRound = 1;
+        curCost = 10;
+    } 
+
+    // Else render table with last stats
     else {
         cStats = JSON.parse(cStats);
+        curRound = parseInt(curRound, 10);
+        curCost = parseInt(curCost, 10);
         for (var player in cStats) {
             createPlayerRow(player, cStats[player].scores);
         }
     }
 
+    // linking functions to buttons
     $('#addPlayer').click(addPlayer);
     $('#clearResults').click(clearResults);
     $('#nextQuestion').click(nextQuestion);
     updateRoundInfo();
-
 }
