ae8d7481ae115b5b4fad3dff97ba60265a137b13
Filat Astakhov <astakhovfilat@gmail.com>
2018-12-21 14:39:47 +0500

    Доработка адаптации

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err);
     }
-    console.log('server is listening on ${port}');
+    console.log('server is listening on {port}');
 })
