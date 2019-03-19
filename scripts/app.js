4bc356f708535058e56ab32bbc2f1b5a8e146e9d
Filat Astakhov <astakhovfilat@gmail.com>
2019-03-19 18:17:16 +0500

    Небольшой рефакторинг и доработка функциональности

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,4 @@
 var cStats = {};
-var curRound = 1;
 var curCost = 10;
 
 
@@ -38,9 +37,9 @@ function createBlankTable() {
     blankTable.append(blankTableTitle).append(blankTableDesc);
     playerlist.append(blankTable);
 
-    const t1 = new TimelineLite({ paused: true });
+    const animation = new TimelineLite({ paused: true });
 
-    t1.staggerFromTo(".blankTable *", 0.5, {
+    animation.staggerFromTo(".blankTable *", 0.5, {
         opacity: 0,
         y: -10
     }, {
@@ -48,90 +47,58 @@ function createBlankTable() {
             y: 0
         }, 0.1)
 
-    t1.play();
+    animation.play();
 }
 
+
+// create scores div with plus or minus scores
 function createScoresDiv(playerId, mode) {
-    console.log('!')
-    var scoresDiv = $('<div>').prop({ id: 'scoresDiv' }).addClass('scores-div');
+    var scoresDivId = 'scoresDiv_' + playerId;
+    var scoresDiv = $('<div>').prop({ id: scoresDivId }).addClass('scores-div');
     var scoresUl = $('<ul>').addClass('scores-list');
-    var scores10 = $('<li>').prop({ cost: '10' }).text(10);
-    var scores20 = $('<li>').prop({ cost: '20' }).text(20);
-    var scores30 = $('<li>').prop({ cost: '30' }).text(30);
-    var scores40 = $('<li>').prop({ cost: '40' }).text(40);
-    var scores50 = $('<li>').prop({ cost: '50' }).text(50);
+    var scores10 = $('<li>').data({ cost: '10' }).text(10);
+    var scores20 = $('<li>').data({ cost: '20' }).text(20);
+    var scores30 = $('<li>').data({ cost: '30' }).text(30);
+    var scores40 = $('<li>').data({ cost: '40' }).text(40);
+    var scores50 = $('<li>').data({ cost: '50' }).text(50);
 
     (mode === 'plus') ? scoresUl.addClass('plus') : scoresUl.addClass('minus');
 
-    scoresUl.append([scores10, scores20, scores30, scores40, scores50]);
+
+    var scoresCosts = [scores10, scores20, scores30, scores40, scores50];
+
+    scoresUl.append(scoresCosts);
     scoresDiv.append(scoresUl);
 
 
-    const flipAnimation = new TimelineLite({
-        paused: true, onComplete: function () {
-
+    const changeAnimation = new TimelineLite({
+        paused: true,
+        onComplete: function () {
+            animation = this;
+            $.each(scoresCosts, function (index, el) {
+                el.on('click', function () {
+                    (mode === 'plus') ? scorePlus(playerId, el.data('cost')) : scoreMinus(playerId, el.data('cost'));
+                    $('#' + scoresDivId).fadeOut(150);        
+                    animation.reverse();
+                    $('#' + scoresDivId).remove();        
+                });
+            });
+            $('#' + playerId).append(scoresDiv);
+            $('#' + scoresDivId).fadeIn(150);
+        
         }
     });
 
-    const flipAnimationSecond = new TimelineLite({
-    });
-    flipAnimationSecond.fromTo("#scoresDiv", 0.5, {
-        x: -300,
-        opacity: 0
-    }, {
-            x: 0,
-            opacity: 1
-        }, 0.5)
-    flipAnimationSecond.play();
-
-
-
-    flipAnimation.to("#" + playerId, 0.25, {
-        x: 300,
+    changeAnimation.staggerTo($('#' + playerId).children(), 0.1, {
         opacity: 0,
         display: 'none'
-    })
+    });
 
+    changeAnimation.play();
 
-
-    flipAnimation.play();
-    $('#' + playerId).before(scoresDiv);
-    flipAnimationSecond.play();
-
-
-
-
-    // if ($('#scoresDiv').length > 0) {
-    //     var animation = new TimelineLite({paused: true});
-    //     animation.fromTo("#scoresDiv", 0.15, {
-    //         height: 60,
-    //         y: 0,
-    //         opacity: 1
-    //     },{
-    //         height: 0,
-    //         y: -70,
-    //         opacity: 0
-    //     });
-    //     animation.play();
-    //     $('#scoresDiv').remove();
-    // }
-
-    // $('#'+playerId).after(scoresDiv);
-    // var animation = new TimelineLite({paused: true});        
-    // animation.fromTo("#scoresDiv", 0.15, {
-    //     height: 0,
-    //     y: -70,
-    //     opacity: 0
-    // },{
-    //     height: 60,
-    //     y: 0,
-    //     opacity: 1
-    // });
-
-    // console.log('scoresDiv', );
-    // animation.play();
 
 }
+
 
 // creating playerDiv with plus/minus button, player name and player scores
 function createPlayerRow(playerName, playerScores) {
@@ -219,19 +186,15 @@ function addPlayer(name) {
 
 // clearing all results from cStats and writing it to Cookie
 function clearResults() {
-
     const t1 = new TimelineLite({
         paused: true,
         onComplete: function () {
             clearPlayerList();
             cStats = {};
-            curRound = 1;
-            setCost(10);
             setCookie('stats', JSON.stringify(cStats), 2);
-            updateRoundInfo();
         }
     });
-    t1.staggerFromTo(".playerDiv", 0.5,
+    t1.staggerFromTo(".playerDiv", 0.15,
         {
             opacity: 1,
             x: 0,
@@ -239,51 +202,31 @@ function clearResults() {
         },
         {
             opacity: 0,
-            x: -70
+            x: -300
         }, -0.1
     );
-
     t1.play();
 }
 
 
 // adding current question cost to current player results 
-function scorePlus(e) {
-    cStats[e.target.parentNode.id].scores += parseInt(curCost);
-    results = parseInt(cStats[e.target.parentNode.id].scores);
-    $(e.target).prev().text(results);
+function scorePlus(playerId, cost) {
+    cStats[playerId].scores += parseInt(cost);
+    results = parseInt(cStats[playerId].scores);
+    $('#' + playerId + ' .playerScores').first().text(results);
     setCookie('stats', JSON.stringify(cStats), 2);
 }
 
 
 // adding current question cost to current player results 
-function scoreMinus(e) {
-    cStats[e.target.parentNode.id].scores -= parseInt(curCost);
-    results = parseInt(cStats[e.target.parentNode.id].scores);
-    $(e.target).nextAll('.playerScores').text(results);
+function scoreMinus(playerId, cost) {
+    cStats[playerId].scores -= parseInt(cost);
+    results = parseInt(cStats[playerId].scores);
+    $('#' + playerId + ' .playerScores').first().text(results);
     setCookie('stats', JSON.stringify(cStats), 2);
 }
 
-
-// updateing text and setting in Cookie
-function updateRoundInfo() {
-    $('#statusRoundSpan').text(curRound);
-    $('#statusCostSpan').text(curCost);
-    setCookie('round', curRound, 2);
-    setCookie('cost', curCost, 2);
-}
-
-
-// change cost and if cost 50 increment the round
-function nextQuestion() {
-    if (curCost < 50) setCost(parseInt(curCost, 10) + 10);
-    else {
-        setCost(10);
-        curRound += 1;
-    }
-    updateRoundInfo();
-}
-
+// Show settings panel
 function showSettings(e, timeline) {
     var settingsButton = $(e.target);
     var gearIcon = $('.button-icon.settings').first();
@@ -302,17 +245,7 @@ function showSettings(e, timeline) {
 
 }
 
-function setCost(cost) {
-    curCost = cost;
-    $('.cost-selector').each(function (index, el) {
-        $(el).removeClass('active');
-
-    });
-    $('li[cost="' + cost + '"]').addClass('active');
-    $('#statusCostSpan').text(cost);
-    setCookie('cost', cost, 2);
-}
-
+// Create Modal with Input to input new player name
 function createInputModal(text) {
     var modalDiv = $('<div>').addClass('modal').prop({ id: 'modal' });
     var modalBg = $('<div>').addClass('modal-bg');
@@ -360,14 +293,13 @@ function createInputModal(text) {
 
     modalInput.on('keyup', function (e, timeline) {
         var input = $(e.target);
-
         if (input.val().length > 0) {
             if (input.val().search(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/) < 0 && input.val().trim().length > 0) {
                 if (!input.hasClass('good')) {
                     input.addClass('good');
                     t0.play();
-                    animateGoodBorder.play();
                 }
+                animateGoodBorder.play();
             }
             else {
                 if (input.hasClass('good')) {
@@ -384,30 +316,6 @@ function createInputModal(text) {
             t0.reverse();
             animateGoodBorder.reverse();
         }
-
-
-        // if (input.val().length > 0) {
-        //     if (input.val().search(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/) < 0 && !input.hasClass('good')) {
-        //         input.addClass('good');
-        //         t0.play();
-        //         animateGoodBorder.play();
-        //     }
-        //     else {
-        //         input.removeClass('good');
-        //         t0.reverse();
-        //         animateGoodBorder.reverse();
-        //     }
-        // }
-
-        // if (input.val().search(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/)) {
-
-        // }
-        // else if (input.val().length > 0 && ) {
-
-        // }
-        // else if (input.val().length < 1 && input.hasClass('good')) {
-
-        // }
 
     });
 
@@ -441,9 +349,11 @@ function createInputModal(text) {
         }
         else {
             animatedTextChange("Такое имя игрока уже существует", '.modal-text', 0.15);
+            animateGoodBorder.reverse();
             setTimeout(function () {
                 animatedTextChange("Введите имя нового игрока", '.modal-text', 0.15);
-            }, 2000);
+                animateGoodBorder.play();
+            }, 1500);
         }
     })
 
@@ -674,8 +584,6 @@ function makeScreenshot() {
 window.onload = function () {
     // Getting last session info
     cStats = getCookie('stats');
-    curRound = getCookie('round');
-    setCost(getCookie('cost'));
 
     // If not remember: start new game
     if (cStats == "" || cStats == "{}" || !cStats) {
@@ -689,17 +597,6 @@ window.onload = function () {
         for (var player in cStats) {
             createPlayerRow(player, cStats[player].scores);
         }
-    }
-
-    if (curRound == "" || !curRound || curCost == "" || !curCost) {
-        curRound = 1;
-        setCost(10);
-
-    }
-
-    else {
-        curRound = parseInt(curRound, 10);
-        setCost(parseInt(curCost, 10));
     }
 
     // Animation
@@ -729,11 +626,4 @@ window.onload = function () {
     $('#addPlayer').click(createInputModal);
     $('#clearResults').click(createConfirmModal);
     $('#screenShot').click(makeScreenshot);
-    $('#nextQuestion').click(nextQuestion);
-    $('.cost-selector').each(function (index, el) {
-        $(el).click(function () {
-            setCost($(el).attr('cost'));
-        });
-    });
-    updateRoundInfo();
 }
